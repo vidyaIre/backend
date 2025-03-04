f1dd61188b916920b2fae741eb6ca1beeb4d0c31
vidya <vidya.palliyalil@gmail.com>
2025-03-04 23:25:46 +0530

    Add new routes for about and user pages in the Express server

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const app = express();
 app.get('/', (req, res) =>{
     res.status(200).send('Hello Express !');
 });
+app.get('/about', (req, res) =>{
+    res.status(200).send('Hello  Now you are in about page!');
+});
+app.get('/user', (req, res) =>{
+    res.status(200).send('Hello  user vidya!');
+});
+
 const PORT = 5000;
 app.listen(PORT, () =>{
     console.log(`Server is running on PORT ${PORT}`);
